1ace916d771ff4142d0123772e0c24fcdc27d3a2
corviato1 <44780076+corviato1@users.noreply.github.com>
2020-10-29 10:03:35 -0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,40 +1,76 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import CreateBucket from './components/CreateBucket';
+import CreateFolder from './components/CreateFolder';
+import UploadFiles from './components/UploadFiles';
+import OpenFile from './components/OpenFile';
+import JoinBucket from './components/JoinBucket';
+import GenerateKeyPair from './components/GenerateKeyPair';
+//Below imports added by Mike
 import logo from './logo.svg';
 import './App.css';
-import Qm from './components/Qm'
+import Qm from './components/Qm';
+
+
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 function App() {
   return (
-    
+    <ThemeProvider theme={darkTheme}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+       <header className="App-header">
+       <a>
           Store the record of your vote on <a href="https://www.urbandictionary.com/define.php?term=Web3">Web3</a>
-        </p>
-        <a
+       </a>
+       <p>
+
+       </p>
+         <img src={logo} className="App-logo" alt="logo" />
+         <p>
+
+         </p>
+         <a
           className="App-link"
-          href="ALEX-LINK-IN"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Upload Your Vote
+          href="<GenerateKeyPair />"
+          target="<GenerateKeyPair />"
+          rel="<GenerateKeyPair />"
+         >Generate/Activate Login Key
         </a>
-        <a
+         <a
           className="App-link"
-          href="ALEX-LINK-OUT"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          View Your Vote
+          href="<UploadFiles />"
+          target="<UploadFiles />"
+          rel="<UploadFiles />"
+         >Upload Your Vote
+          </a>
+         <a
+           className="App-link"
+           href="<OpenFile />"
+           target="<OpenFile />"
+            rel="<OpenFile />"
+          >View Your Vote
+           </a>
+		<a
+          className="App-link"
+          href=" <CreateBucket />"
+          target=" <CreateBucket />"
+          rel=" <CreateBucket />"
+         >View Blockchain Link
         </a>
-      </header>
-      <body>
-        <div>
-          <Qm></Qm>
-        </div>
-      </body>
+          </header>
     </div>
+	<body>
+		<div>
+			<Qm></Qm>
+		</div>
+  </body>
+    </ThemeProvider>
   );
 }
 
